d21360881b63b7ff79d03e64374479294b71769f
Aruneko <aruneko99@gmail.com>
2018-04-08 23:56:09 +0900

    ページ数調節のためちょっと追記

--- assets/aruneko/test_seq.py
+++ assets/aruneko/test_seq.py
@@ -1,3 +1,27 @@
-s = Seq(1, 2, 3, 4, 5).map(lambda x: x + 1)\
-                      .flat_map(lambda x: [x, x])
-print(s)  # => [2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
+import unittest
+from functionalpy import Seq
+
+
+def test_monad_1st_low(self):
+    seq = Seq(1)
+
+    def f(x):
+        return Seq(x + 1)
+
+    self.assertEqual(seq.flat_map(f), f(1))
+
+
+def test_monad_2nd_low(self):
+    seq = Seq(1, 2, 3, 4, 5)
+
+    self.assertEqual(seq.flat_map(Seq), seq)
+
+
+def test_monad_3rd_low(self):
+    seq = Seq(1, 2, 3, 4, 5)
+
+    def f(x):
+        return Seq(x + 1)
+
+    def g(x):
+        return Seq(x * 2)
