a6427803ed34a15b99b2704bcb5b70edc66d1ab3
hyoiutu <ke-suke.flandre@ezweb.ne.jp>
2018-04-12 16:55:55 +0900

    記事が完成した

--- assets/keisuke495500asset/latex_freq.py
+++ assets/keisuke495500asset/latex_freq.py
@@ -1,43 +1,20 @@
-from latex_lcg import lcg
-from latex_lcg import RAND_MAX
+from latex_lcg import lcg,RAND_MAX
 from collections import defaultdict
 
-# <= 0.1 : A
-# <= 0.2 : B
-# <= 0.3 : C
-# <= 0.4 : D
-# <= 0.5 : E
-# <= 0.6 : F
-# <= 0.7 : G
-# <= 0.8 : H
-# <= 0.9 : I
-# <= 1.0 : J
 def classifier(random_number):
-    if random_number <= 0.1:
-        return "A"
-    elif random_number <= 0.2:
-        return "B"
-    elif random_number <= 0.3:
-        return "C"
-    elif random_number <= 0.4:
-        return "D"
-    elif random_number <= 0.5:
-        return "E"
-    elif random_number <= 0.6:
-        return "F"
-    elif random_number <= 0.7:
-        return "G"
-    elif random_number <= 0.8:
-        return "H"
-    elif random_number <= 0.9:
-        return "I"
-    else:
-        return "J"
+    for i,x in zip(range(1,11), \
+                 ["A","B","C","D","E","F","G","H","I","J"]):
+        if random_number <= i*0.1:
+            return x
 
 gen = lcg()
 n = 200000
+chi = 0
 real_freqs = defaultdict(lambda: 0)
 for i in range(0,n):
     real_freqs[classifier(next(gen)/RAND_MAX)] += 1
+
 for i,x in real_freqs.items():
+    chi += (x - 20000)*(x - 20000)/20000
     print(f"{i}:{x}")
+print(f"chi = {chi}")
